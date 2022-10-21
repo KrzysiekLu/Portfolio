701f5e3755bc0f6391244b1655e3b244fe957676
KrzysiekLu <krzysztof.luuczak@gmail.com>
2022-10-21 12:36:56 +0200

    Adding functionality to copy contact information

--- src/scripts/copyContactDetails.js
+++ src/scripts/copyContactDetails.js
@@ -1,13 +1,12 @@
 export const copyContactDetails = () => {
   const copyLinks = document.querySelectorAll(" .wraper >.copy");
-  const emailAdress = document.querySelector(".email__adress");
-  copyLinks[0].addEventListener("click", (e) => {
-    console.log(e.target);
-    navigator.clipboard
-      .readText()
-      .then(
-        (clipText) => (document.querySelector(".editor").innerText += clipText)
+
+  copyLinks.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      navigator.clipboard.writeText(
+        e.target.previousElementSibling.textContent
       );
+    });
   });
 };
 
