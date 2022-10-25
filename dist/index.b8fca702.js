5e1485e2739eb2a99ef6df094978bb508dd7d1bb
KrzysiekLu <krzysztof.luuczak@gmail.com>
2022-10-25 14:52:07 +0200

    adding confirmation message

--- dist/index.b8fca702.js
+++ dist/index.b8fca702.js
@@ -550,7 +550,12 @@ const handleSubmit = (event)=>{
             "Content-Type": "application/x-www-form-urlencoded"
         },
         body: new URLSearchParams(formData).toString()
-    }).then(()=>console.log("Form successfully submitted")).catch((error)=>alert(error));
+    }).then(()=>{
+        document.querySelector(".contact__overlay").classList.add("contact__overlay--open");
+        document.querySelector(".confirm-message__button").addEventListener("click", ()=>{
+            document.querySelector(".contact__overlay").classList.remove("contact__overlay--open");
+        });
+    }).catch((error)=>alert(error));
 };
 document.querySelector("form").addEventListener("submit", handleSubmit);
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,7 +18,18 @@ const handleSubmit = (event) => {
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
   })
-    .then(() => console.log("Form successfully submitted"))
+    .then(() => {
+      document
+        .querySelector(".contact__overlay")
+        .classList.add("contact__overlay--open");
+      document
+        .querySelector(".confirm-message__button")
+        .addEventListener("click", () => {
+          document
+            .querySelector(".contact__overlay")
+            .classList.remove("contact__overlay--open");
+        });
+    })
     .catch((error) => alert(error));
 };
 
